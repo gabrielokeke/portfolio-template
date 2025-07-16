1b051b3e6b38f4d92a7361e206392ea1f592f03b
gabrielokeke <okekegabriel84@gmail.com>
2025-07-16 12:47:37 +0100

    [ADDED] favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Jane's Portfolio",
+  title: "Your Name's Portfolio",
   description: "A brief portfolio",
 };
 
